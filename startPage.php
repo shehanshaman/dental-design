c1a98e2a4f2628f0364d4087bede11de1e06ad29
chamara1995 <kggcbgunarathne@gmail.com>
2017-11-15 11:02:40 +0530

    Add files .css .js and startPage.php

---
+++ startPage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width", initial-scale=1.0>
+	<link rel="stylesheet" href="css/bootsrapnew.css">
+	<link rel="stylesheet" href="css/startPage.css">	
+</head>
+
+<body>
+
+<form class="form_class">
+	
+	<div class="container">
+		 
+		<div class="row">		
+			<div class="col-sm-4">Complaint :</div>
+			<div class="col-sm-8">
+				<input type="text" class="form-control" name="input_Complaint">
+			</div>
+		</div>
+	
+		<div class="row">		
+			<div class="col-sm-4">Type :</div>
+			<div class="col-sm-8">
+				<input type="text"class="form-control" name="input_type">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-12">
+				History of presenting complaint :
+			</div>
+		</div>
+
+	
+		<div class="row">		
+			<div class="col-sm-4"></div>
+			<div class="col-sm-8">
+				<textarea id="last_text_box" class="form-control"></textarea>
+			</div>
+		</div>
+	</div>
+
+	<center>
+		<div class="row">
+			<div class="col-sm-12">
+				<button style="width: 250px; height: 50px;" class="btn btn-success">Enter</button>
+			</div>
+		</div>
+	</center>
+
+
+</form>
+
+
+</body>
+
+</html>
